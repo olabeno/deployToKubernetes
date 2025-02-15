d1d6c361f15f850197b8ff0e1da16fe0093bfc80
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-02-15 22:27:17 +0100

    Another update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Welcome to Git Actions with CI CD pipeline. Watch git actions take place after you commit changes here"
+echo "Welcome to Git Actions again with CI CD pipeline. Watch git actions take place after you commit changes here"
